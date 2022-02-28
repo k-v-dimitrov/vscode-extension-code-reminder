d8e13a4cccab2c1d7d0b8ab5ae557ae9c70acc09
Kiril Dimitrov <kiri.dimitrov@mkit.io>
2022-02-28 14:26:02 +0200

    feat: basic commands, pushSubscriptions handler, remove comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,10 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { RemindersProvider } from "./services/reminders-provider";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "code-remind" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const cmdHelloWorldDisposable = vscode.commands.registerCommand(
-    "code-remind.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from Code Remind!");
-    }
-  );
+  const remindersProviderInstance = new RemindersProvider(context);
 
   const cmdSetReminder = vscode.commands.registerCommand(
     "code-remind.setReminder",
@@ -30,9 +15,24 @@ export function activate(context: vscode.ExtensionContext) {
     }
   );
 
-  context.subscriptions.push(cmdHelloWorldDisposable);
-  context.subscriptions.push(cmdSetReminder);
+  const cmdGetReminders = vscode.commands.registerCommand(
+    "code-remind.getReminders",
+    () => {
+      vscode.window.showInformationMessage(
+        "Reminders: ",
+        JSON.stringify(remindersProviderInstance.reminders, null, 4)
+      );
+    }
+  );
+
+  pushSubscriptions(context, [cmdSetReminder, cmdGetReminders]);
 }
 
-// this method is called when your extension is deactivated
+function pushSubscriptions(
+  context: vscode.ExtensionContext,
+  subscriptions: Array<vscode.Disposable>
+) {
+  context.subscriptions.push(...subscriptions);
+}
+
 export function deactivate() {}

e4849554ea9bfcd85fa4d44c62bd76f037559971
Kiril Dimitrov <kiril.dimitrov@mkit.io>
2022-05-29 19:06:54 +0000

    feat: snooze reminders

--- src/models/notification.ts
+++ src/models/notification.ts
@@ -1,6 +1,9 @@
+import deleteReminder from "../handlers/delete-reminder.handler";
 import * as vscode from "vscode";
 
 import { globalEvents } from "../extension";
+import Reminder from "./reminder";
+import updateReminder from "../handlers/update-reminder.handler";
 
 abstract class Notification {
   async showReminderCTA(title: string, content: string, choiceMap: string[]) {
@@ -13,8 +16,6 @@ abstract class Notification {
         title: reminderText,
         placeHolder: choiceMap[0],
       }),
-
-      vscode.window.showInformationMessage(reminderText, ...choiceMap),
     ]);
 
     if (choice) {
@@ -32,26 +33,65 @@ abstract class Notification {
 export class GoToFileNotification extends Notification {
   constructor(
     private readonly notificationTitle: string,
-    private readonly notificationContent: string,
+    private readonly reminder: Reminder,
     private readonly fileLocation: string,
-    private readonly choiceMap: string[] = ["Open file", "Resolved"]
+    private readonly choiceMap: string[] = [
+      "Open file",
+      "Resolved",
+      "Notify Again in 1 minutes",
+      "Notify Again in 5 minutes",
+      "Notify Again in 10 minutes",
+    ]
   ) {
     super();
-    this.showReminderCTA(notificationTitle, notificationContent, choiceMap);
+    this.showReminderCTA(notificationTitle, reminder.name, choiceMap);
   }
 
   choiceHandler = async (selectedChoice: string) => {
     switch (selectedChoice) {
       case "Open file":
-        console.log("opens file");
-
-        let doc = await vscode.workspace.openTextDocument(this.fileLocation); // calls back into the provider
-        await vscode.window.showTextDocument(doc, { preview: false });
-
+        await this.openReminderTextFile();
+        deleteReminder(this.reminder.id);
         break;
 
       case "Resolved":
+        deleteReminder(this.reminder.id);
+        break;
+
+      case "Notify Again in 1 minutes":
+        updateReminder(this.reminder.id, {
+          wasNotificationShown: false,
+          date: this.addMinutesToDate(this.reminder.date, 1),
+        });
+
+        break;
+
+      case "Notify Again in 5 minutes":
+        updateReminder(this.reminder.id, {
+          wasNotificationShown: false,
+          date: this.addMinutesToDate(this.reminder.date, 5),
+        });
+
+        break;
+
+      case "Notify Again in 10 minutes":
+        updateReminder(this.reminder.id, {
+          wasNotificationShown: false,
+          date: this.addMinutesToDate(this.reminder.date, 10),
+        });
+
         break;
     }
+
+    globalEvents.emit("refresh-tree-data");
   };
+
+  private async openReminderTextFile() {
+    let doc = await vscode.workspace.openTextDocument(this.fileLocation); // calls back into the provider
+    await vscode.window.showTextDocument(doc, { preview: false });
+  }
+
+  addMinutesToDate(date: Date, minutes: number) {
+    return new Date(date.getTime() + minutes * 60000);
+  }
 }
